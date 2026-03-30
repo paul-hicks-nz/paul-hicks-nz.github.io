1752dddf7a4c5779b2d674423f5ecadc19b1d125
Paul Hicks <92405702+paul-hicks-nz@users.noreply.github.com>
2026-03-31 12:16:19 +1300

    Add Jest configuration for TypeScript and React
    Configured Jest for TypeScript and React testing.

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx'
+      }
+    }]
+  }
+}

6c96eb0b9a77edd4c9df134f0f9f0492285930e7
Paul Hicks <paul.hicks@sentify.co>
2026-04-01 16:45:30 +1300

    Tests running even better

--- packages/test/site/bun-test.d.ts
+++ packages/test/site/bun-test.d.ts
@@ -1,7 +1,9 @@
 import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+import { Matchers, AsymmetricMatchers } from "bun:test";
 
 declare module "bun:test" {
   // We interface merge with Bun's internal Matchers interface
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface Matchers<T = unknown> extends TestingLibraryMatchers<typeof expect.stringContaining, T> { }
+  interface Matchers<T> extends TestingLibraryMatchers<typeof expect.stringContaining, T> { }
+  interface AsymmetricMatchers extends TestingLibraryMatchers { }
 }
